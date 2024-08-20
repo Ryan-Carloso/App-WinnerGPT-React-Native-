d95bbb30ecaae098c840892eee8684b7242a6598
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-08-20 09:45:57 +0100

    add more resposnsive header

--- components/header.js
+++ components/header.js
@@ -1,46 +1,46 @@
-// components/header.js
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, TouchableOpacity, Text, Picker, useWindowDimensions, Image } from 'react-native';
-import { Picker as RNPicker } from '@react-native-picker/picker'; // Importa o Picker
+import { StyleSheet, View, TouchableOpacity, Text, Image, useWindowDimensions, TextInput } from 'react-native';
+import { Dimensions } from 'react-native';
 
-const Header = ({ selectedTeam, setSelectedTeam, sortOrder, setSortOrder, selectedLeague, setSelectedLeague }) => {
+// Obtendo as dimensões da tela
+const { width: screenWidth } = Dimensions.get('window');
+
+const fontSizeBase = screenWidth < 360 ? 14 : screenWidth < 700 ? 16 : 18;
+const titleFontSize = screenWidth < 360 ? 20 : screenWidth < 700 ? 24 : 28;
+const winnerFontSize = screenWidth < 360 ? 16 : screenWidth < 700 ? 18 : 20;
+const dropdownTop = screenWidth >= 768 ? 50 : 85;
+const dropdownRight = screenWidth >= 768 ? 10 : 40;
+
+
+const Header = ({ selectedLeague, setSelectedLeague, selectedTeam, setSelectedTeam }) => {
   const { width } = useWindowDimensions();
-  const isMobile = width < 768;
-  const [isPickerVisible, setIsPickerVisible] = useState(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
-  const togglePickerVisibility = () => {
-    setIsPickerVisible(prevState => !prevState);
+  const toggleDropdownVisibility = () => {
+    setIsDropdownVisible(prevState => !prevState);
+  };
+
+  const selectLeague = (league) => {
+    setSelectedLeague(league);
+    setIsDropdownVisible(false);  // Fecha o dropdown após a seleção
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
+      <View style={[styles.header, width >= 768 && styles.headerTablet]}>
         <View style={styles.containerImage}>
-          <Text style={styles.text}>Winner GPT</Text>
           <Image
             style={styles.tinyLogo}
             source={require('../assets/icon.png')}
           />
+          <Text style={styles.text}>Winner GPT</Text>
         </View>
-        <TouchableOpacity onPress={togglePickerVisibility} style={styles.button}>
-          <Text style={styles.buttonText}>Select League</Text>
+        
+        <TouchableOpacity onPress={toggleDropdownVisibility} style={styles.button}>
+          <Text style={styles.buttonText}>Filters ▼</Text>
         </TouchableOpacity>
       </View>
-      {isPickerVisible && (
-        <View style={styles.pickerContainer}>
-          <RNPicker
-            selectedValue={selectedLeague}
-            style={styles.picker}
-            onValueChange={(itemValue) => setSelectedLeague(itemValue)}
-          >
-            <RNPicker.Item label="All Leagues" value="all" />
-            <RNPicker.Item label="Premier League" value="premierleague" />
-            <RNPicker.Item label="Champions League" value="championsleague" />
-            <RNPicker.Item label="Liga Portugal" value="ligaportugal" />
-          </RNPicker>
-        </View>
-      )}
-      <View style={styles.topBar}>
+      <View style={[styles.topBar, width >= 768 && styles.topBarTablet]}>
         <TextInput
           style={styles.input}
           placeholder="Enter team name"
@@ -48,6 +48,19 @@ const Header = ({ selectedTeam, setSelectedTeam, sortOrder, setSortOrder, select
           onChangeText={setSelectedTeam}
         />
       </View>
+      {isDropdownVisible && (
+        <View style={styles.dropdown}>
+          <TouchableOpacity onPress={() => selectLeague('premierleague')} style={styles.dropdownItem}>
+            <Text>Premier League</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => selectLeague('championsleague')} style={styles.dropdownItem}>
+            <Text>Champions League</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => selectLeague('ligaportugal')} style={styles.dropdownItem}>
+            <Text>Liga Portugal</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -58,36 +71,70 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
+  },
+  headerTablet: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   containerImage: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   text: {
-    fontSize: 24,
+    fontSize: titleFontSize , // Ajusta dinamicamente o tamanho da fonte
     fontWeight: 'bold',
+    marginLeft: 10,
+    marginRight: 30,
   },
   tinyLogo: {
-    width: 70,
-    height: 70,
-  },
-  pickerContainer: {
-    marginVertical: 10,
-    maxWidth: 200,
-  },
-  picker: {
+    width: 40,
     height: 40,
-    width: '100%',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#999999',
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: 150,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: fontSizeBase, // Ajusta dinamicamente o tamanho da fonte do botão
+  },
+  dropdown: {
+    position: 'absolute',
+    top: dropdownTop, // Altera a posição do dropdown com base na largura da tela
+    right: dropdownRight,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex: 1000, // Garante que o dropdown apareça acima de outros conteúdos
+    width: 150,
+  },
+  dropdownItem: {
+    padding: 10,
+    borderBottomColor: '#999999',
+    borderBottomWidth: 1,
   },
   topBar: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: 'column',
     alignItems: 'center',
-    marginBottom: 10,
+    marginTop: 10, // Adiciona algum espaçamento em relação ao dropdown
+  },
+  topBarTablet: {
+    marginTop: 10,
+    justifyContent: 'center',
   },
   input: {
     height: 40,
@@ -101,16 +148,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    fontSize: 20,
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'gray',
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+    fontSize: winnerFontSize, // Ajusta dinamicamente o tamanho da fonte do input
   },
 });
 
